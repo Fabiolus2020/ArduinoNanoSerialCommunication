259cba602b24ae7006901ba082b4f881d881e05d
Fabiolus2020 <76980761+Fabiolus2020@users.noreply.github.com>
2021-01-05 16:59:59 -0500

    Add files via upload

---
+++ NanoSerialCommInputKeyboardInteractionMenu.ino
@@ -0,0 +1,33 @@
+
+int BlinkTime;
+int RedLedPin = 3;
+
+
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(RedLedPin, OUTPUT);
+  digitalWrite(RedLedPin, LOW);
+
+}
+
+
+void loop() {
+  Serial.println("How many time do you want the LED to blink?");
+
+  while (Serial.available() == 0); {}
+
+  BlinkTime = Serial.parseInt();
+
+  Serial.print("You have chosen number: ");
+  Serial.println(BlinkTime);
+  delay(1000);
+
+  for (int counter = 1; counter <= BlinkTime; counter++) {
+    digitalWrite(RedLedPin, HIGH);
+    delay(1000);
+    digitalWrite(RedLedPin, LOW);
+    delay(1000);
+  }
+
+}
